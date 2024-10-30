894d36c128b8b3a27978210898d7a7a325e0711a
Johannes Brodwall <johannes@brodwall.com>
2024-10-30 15:42:45 +0100

    feature: show tasks (rendered on client)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<h1>My task app</h1>);
+root.render(<Application />);
+
+interface Task {
+  description: string;
+  completed: boolean;
+}
+function Application() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  async function loadTasks() {
+    const tasks = [
+      { description: "get it to work", completed: true },
+      { description: "move tasks to server", completed: false },
+      { description: "deploy to heroku", completed: false },
+    ];
+    setTasks(tasks);
+  }
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>My tasks</h1>
+      {tasks.map((task) => (
+        <div key={task.description}>
+          <input type={"checkbox"} checked={task.completed} />
+          {task.description}
+        </div>
+      ))}
+    </>
+  );
+}
